538096bc5a5a1946bb34c5612601a6344e2d3954
MaxKho <33136225+MaxKho@users.noreply.github.com>
2025-08-18 20:40:31 +0100

    Preliminary version of package

--- src/scape/scape_t/__init__.py
+++ src/scape/scape_t/__init__.py
@@ -3,23 +3,9 @@ SCAPE-T: Semantic Complexity from Attention Patterns in Encoders - encoder-only 
 """
 
 from .core import get_model, get_tokeniser, device, scape_t
-from .datasets import (
-    get_all_datasets, get_dev_splits, get_test_splits,
-    get_gau_split, get_abstractness_dict
-)
-from .eval import pairwise_accuracy, build_results_table
-from .ablations import (
-    run_submetric_ablation_dev, run_layer_ablation_dev,
-    run_submetric_ablation_4v12, draw_layer_boxes
-)
-from .hyperopt import evolutionary_search
 from .resources import ABBREVIATIONS, DELIMITERS, STOPWORDS, PUNCT, DEFAULT_PARAMS
 
 __all__ = [
-    "scapet", "pairwise_accuracy",
-    "get_all_datasets", "get_dev_splits", "get_test_splits", "get_gau_split", "get_abstractness_dict",
-    "build_results_table",
-    "run_submetric_ablation_dev", "run_layer_ablation_dev", "run_submetric_ablation_4v12", "draw_layer_boxes",
-    "evolutionary_search",
+    "scape_t", "get_model", "get_tokeniser", "device",
     "ABBREVIATIONS", "DELIMITERS", "STOPWORDS", "PUNCT", "DEFAULT_PARAMS"
 ]
