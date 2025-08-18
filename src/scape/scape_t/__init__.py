88dc7fa2ffebc4956bbfa1ea3f56417290f6abd2
MaxKho <33136225+MaxKho@users.noreply.github.com>
2025-08-19 00:21:23 +0100

    Update __init__.py to add bert_token_length

--- src/scape/scape_t/__init__.py
+++ src/scape/scape_t/__init__.py
@@ -4,8 +4,8 @@ SCAPE-T: Semantic Complexity from Attention Patterns in Encoders - encoder-only 
 
 from .core import get_model, get_tokeniser, device, scape_t
 from .resources import ABBREVIATIONS, DELIMITERS, STOPWORDS, PUNCT, DEFAULT_PARAMS, ABSTRACTNESS
-from from .dataset_builder import get_dev_splits, get_test_splits, get_gau_split, get_all_datasets
+from from .dataset_builder import get_dev_splits, get_test_splits, get_gau_split, get_all_datasets, bert_token_length
 
 __all__ = ["get_model", "get_tokeniser", "device", "scape_t",
            "ABBREVIATIONS", "DELIMITERS", "STOPWORDS", "PUNCT", "DEFAULT_PARAMS", "ABSTRACTNESS",
-           "get_dev_splits, get_test_splits, get_gau_split, get_all_datasets"]
+           "get_dev_splits, get_test_splits, get_gau_split, get_all_datasets, bert_token_length"]
