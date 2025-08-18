6fe53c463d86de9fa9357cb3511bb49f7587c3ff
MaxKho <33136225+MaxKho@users.noreply.github.com>
2025-08-19 00:25:45 +0100

    Fix syntax bug

--- src/scape/scape_t/__init__.py
+++ src/scape/scape_t/__init__.py
@@ -4,7 +4,7 @@ SCAPE-T: Semantic Complexity from Attention Patterns in Encoders - encoder-only 
 
 from .core import get_model, get_tokeniser, device, scape_t
 from .resources import ABBREVIATIONS, DELIMITERS, STOPWORDS, PUNCT, DEFAULT_PARAMS, ABSTRACTNESS
-from from .dataset_builder import get_dev_splits, get_test_splits, get_gau_split, get_all_datasets, bert_token_length
+from .dataset_builder import get_dev_splits, get_test_splits, get_gau_split, get_all_datasets, bert_token_length
 
 __all__ = ["get_model", "get_tokeniser", "device", "scape_t",
            "ABBREVIATIONS", "DELIMITERS", "STOPWORDS", "PUNCT", "DEFAULT_PARAMS", "ABSTRACTNESS",
