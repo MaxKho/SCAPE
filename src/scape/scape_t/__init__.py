21e119037c7d54189a88c1e41d85504b941ccc30
MaxKho <33136225+MaxKho@users.noreply.github.com>
2025-08-19 02:16:19 +0100

    Update __init__.py

--- src/scape/scape_t/__init__.py
+++ src/scape/scape_t/__init__.py
@@ -1,11 +1,19 @@
 """
-SCAPE-T: Semantic Complexity from Attention Patterns in Encoders - encoder-only Transformer variant
+SCAPE-T: Semantic Complexity from Attention Patterns in Encoders
+(encoder-only Transformer variant)
 """
 
 from .core import get_model, get_tokeniser, device, scape_t
-from .resources import ABBREVIATIONS, DELIMITERS, STOPWORDS, PUNCT, DEFAULT_PARAMS, ABSTRACTNESS
-from .dataset_builder import get_dev_splits, get_test_splits, get_gau_split, get_all_datasets, bert_token_length
+from .resources import ABBREVIATIONS, DELIMITERS, STOPWORDS, PUNCT, DEFAULT_PARAMS
 
-__all__ = ["get_model", "get_tokeniser", "device", "scape_t",
-           "ABBREVIATIONS", "DELIMITERS", "STOPWORDS", "PUNCT", "DEFAULT_PARAMS", "ABSTRACTNESS",
-           "get_dev_splits, get_test_splits, get_gau_split, get_all_datasets, bert_token_length"]
+__all__ = [
+    "get_model",
+    "get_tokeniser",
+    "device",
+    "scape_t",
+    "ABBREVIATIONS",
+    "DELIMITERS",
+    "STOPWORDS",
+    "PUNCT",
+    "DEFAULT_PARAMS",
+]
